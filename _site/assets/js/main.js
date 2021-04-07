0fee032cb3ef472ecc05adf030f131fe9020cad1
Yitong <yitong.zhang@coinbase.com>
2021-04-06 23:07:38 -0700

    halfway done styling about

---
+++ _site/assets/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.modal-trigger').addEventListener('click', event => {
+      // render the modal
+      console.log("trigger detected")
+    })
+})
