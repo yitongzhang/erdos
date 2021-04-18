3da37a37a72342677d0d8321b532349030317f49
Yitong <yitong.zhang@coinbase.com>
2021-04-18 11:20:18 -0700

    mobile is no longer broken

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.nav-modal-open').addEventListener('click', event => {
       injectAnimationParams('in')
-      openModal('nav')
+      openModal('nav',isMobile)
     })
 })
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.footer-modal-open').addEventListener('click', event => {
     if(event.target != document.getElementById("footer-button")){
       injectAnimationParams('in')
-      openModal('footer')
+      openModal('footer',isMobile)
     }
   })
 })
@@ -21,13 +21,34 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.modal-close-from-icon').addEventListener('click', event => {
+    injectAnimationParams('out')
+    closeModal()
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.close-modal-logo').addEventListener('click', event => {
+    injectAnimationParams('out')
+    closeModal()
+  })
+})
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    injectAnimationParams('out')
+    closeModal()
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.dealTerms-open').addEventListener('click', event => {
     window.scrollTo(0, document.getElementsByClassName('deal')[0].offsetTop)
   })
 })
 
-
-// get modal height
+var isMobile = isMobile(screen.width)
+console.log(isMobile)
 var modal = document.getElementsByClassName("modal")[0];
 var overlay = document.getElementsByClassName("overlay")[0];
 var html = document.documentElement;
@@ -37,7 +58,6 @@ var footerHeight = footer.offsetHeight;
 var modalHeight = modal.offsetHeight;
 var currentModalOpenedFromFooter = false;
 
-console.log(footerHeight);
 function injectAnimationParams(direction) {
   if(direction == 'in'){
     modal.style.transition = "top cubic-bezier(0.190, -0.005, 0.000, 1.000)  600ms, opacity cubic-bezier(0.190, -0.005, 0.000, 1.000)  300ms 300ms";
@@ -54,14 +74,24 @@ function injectAnimationParams(direction) {
 
 }
 
-function openModal (location) {
-    modal.style.top = "calc( 100vh - " + modalHeight + "px )"
+function openModal (location,isMobile) {
+
+    if(isMobile){
+      modal.style.top = "0px";
+      modal.style.left = "0px";
+      modal.style.overflow ="auto"
+      modal.style.height ="100%"
+    }
+    else{
+      modal.style.top = "calc( 100vh - " + modalHeight + "px )"
+    }
+    
     modal.style.opacity="1"
     
     overlay.style.opacity = 1;
     overlay.style.pointerEvents = "auto";
 
-    if(location == "footer"){
+    if(location == "footer" && !isMobile){
       footer.style.height = modalHeight + "px";
       footer.style.bottom = (modalHeight-footerHeight) + "px";
       footerInside.style.opacity = '0'
@@ -70,11 +100,11 @@ function openModal (location) {
     else{
       currentModalOpenedFromFooter = false;
     }
-
-    html.style.overflow ="hidden";
+    // html.style.overflow ="hidden";
 }
 
 function closeModal () {
+  console.log('close')
   modal.style.top = "100vh"
   modal.style.opacity="0"
   
@@ -88,4 +118,11 @@ function closeModal () {
   }
 
   html.style.overflow ="visible";
+}
+
+function isMobile(screenWidth){
+  if(screenWidth < 1200){
+    return true
+  }
+  return false
 }
